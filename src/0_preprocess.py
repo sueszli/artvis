6c97105534c369c16e71b8b7fe451e495767381b
sueszli <jabaryyahya@gmail.com>
2024-10-28 17:43:35 +0100

    up

--- src/0_preprocess.py
+++ src/0_preprocess.py
@@ -1,10 +1,7 @@
 from utils import get_current_dir
 
 inputpath = get_current_dir().parent / "data" / "artvis_dump.csv"
-outputpath = get_current_dir().parent / "data" / "artvis_cleaned_v0.csv"
-
-line_count = 0
-err_count = 0
+outputpath = get_current_dir().parent / "data" / "artvis_cleaned.csv"
 
 
 with open(inputpath, "r") as i:
@@ -29,7 +26,10 @@ with open(inputpath, "r") as i:
                 if "," in linearr[i]:
                     linearr[i] = '"' + linearr[i] + '"'
 
-            # check num of cols
+            """
+            over 10k lines use unquoted delimiters.
+            they can only fixed manually, so we have to drop them.
+            """
             if len(linearr) != numcols:
                 print(f"invalid @ index {index}: expected {numcols} but got {len(linearr)}")
                 tmpheader = header.split(",")
@@ -42,14 +42,7 @@ with open(inputpath, "r") as i:
                 for i, (a, b) in enumerate(zip(tmpheader, tmpline)):
                     print(f"{i:2d}: {a} -> {b}")
                 print("\n")
-                err_count += 1
                 continue
 
             line = ",".join(linearr)
             o.write(line + "\n")
-            line_count += 1
-
-
-# over 10k lines use unquoted delimiters.
-# they can only be fixed manually, so we have to drop them.
-print(f"errors: {err_count}/{line_count}")
